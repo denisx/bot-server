9c4c20fc4d749cffa496c4f89e10f2b47a364e2b
denisx <github@denisx.ru>
2016-06-09 18:19:27 +0300

    reformat vari_ables to camelCase

--- bot-server.js
+++ bot-server.js
@@ -21,30 +21,30 @@ var Bot = function(opts) {
 	console.log('');
 	var self = this;
 	self.dev = opts.dev || false;
-	self.default_lang = opts.default_lang;
+	self.defaultLang = opts.defaultLang;
 	self.services = opts.services;
 	self.settings = opts.settings;
 	self.texts = opts.texts;
-	self.save_queue_params = opts.save_queue_params;
+	self.saveQueueParams = opts.saveQueueParams;
 	self.vars = {
 		keyboard: [], // max 4 * 12 (h*w); you can make more by height, but scroll'll be turned on
-		resize_keyboard: true
+		resizeKeyboard: true
 	};
-	self.save_queue_params = opts.save_queue_params;
+	self.saveQueueParams = opts.saveQueueParams;
 };
 
 Bot.prototype.init = function () {
 	var self = this;
 	var origin = os.hostname();
-	self.settings_machine = self.settings[origin];
+	self.settingsMachine = self.settings[origin];
 	self.bot = new NodeBot({
-		token: self.settings_machine.bot.token
+		token: self.settingsMachine.bot.token
 	});
 	self.menu = {};
 	self.queue = {};
-	self.dev = self.settings_machine.bot.dev || false;
-	self.bot_stoped = true;
-	self.bot.enableAnalytics(self.settings_machine.botan.token);
+	self.dev = self.settingsMachine.bot.dev || false;
+	self.botStoped = true;
+	self.bot.enableAnalytics(self.settingsMachine.botan.token);
 	self.bot
 		/** @param {Object} msg.from */
 		.on('message', function(msg) { // local user event
@@ -56,11 +56,11 @@ Bot.prototype.init = function () {
 			self.menu[id] = self.menu[id] ||
 				{
 					path: '/',
-					keyboard_path: '/',
-					lang: self.default_lang,
-					on_work: false
+					keyboardPath: '/',
+					lang: self.defaultLang,
+					onWork: false
 				};
-			self.menu[id].last_ping = new Date();
+			self.menu[id].lastPing = new Date();
 			self.menu[id].texts = texts[self.menu[id].lang];
 			self.queue[id] = self.queue[id] || [];
 			self.queue[id].push(msg);
@@ -92,12 +92,12 @@ Bot.prototype.init = function () {
 Bot.prototype.clearSessions = function () {
 	var self = this;
 	var now = new Date();
-	var session_minutes = 15;
+	var sessionMinutes = 15;
 	Object.keys(self.menu).forEach(function (id) {
 		var menu = self.menu[id];
-		console.log('check last ping', id, getDT(menu.last_ping - 1), ' || ', getDT((menu.last_ping - 1) + session_minutes * 60 * 1000), ' || ', getDT(now - 1));
-		if ((menu.last_ping - 1) + session_minutes * 60 * 1000 < now - 1) {
-			console.log('clear', id, getDT(menu.last_ping - 1), getDT(now - 1));
+		console.log('check last ping', id, getDT(menu.lastPing - 1), ' || ', getDT((menu.lastPing - 1) + sessionMinutes * 60 * 1000), ' || ', getDT(now - 1));
+		if ((menu.lastPing - 1) + sessionMinutes * 60 * 1000 < now - 1) {
+			console.log('clear', id, getDT(menu.lastPing - 1), getDT(now - 1));
 			delete self.menu[id];
 		}
 	});
@@ -111,26 +111,26 @@ Bot.prototype.stop = function () {
 Bot.prototype.onMessage = function (id) {
 	var self = this;
 	var menu = self.menu[id];
-	if (!menu.on_work && self.queue[id].length) {
+	if (!menu.onWork && self.queue[id].length) {
 		var msg = self.queue[id].shift();
-		menu.on_work = true;
+		menu.onWork = true;
 		menu.msg = msg;
-		var bot_free = true;
-		if (bot_free && msg.text) {
-			bot_free = false;
+		var botFree = true;
+		if (botFree && msg.text) {
+			botFree = false;
 			self.parseText(id);
 		}
-		if (bot_free && msg.location) {
-			bot_free = false;
+		if (botFree && msg.location) {
+			botFree = false;
 			self.parseText(id);
 		}
-		if (bot_free && msg.photo) {
-			bot_free = false;
+		if (botFree && msg.photo) {
+			botFree = false;
 			self.parseText(id);
 		}
-		if (bot_free) {
-			console.error('error', 'bot_free');
-			bot_free = false;
+		if (botFree) {
+			console.error('error', 'botFree');
+			botFree = false;
 			menu.answer = 'no answer :)';
 			self.commandAnswer(id);
 		}
@@ -141,32 +141,32 @@ Bot.prototype.getKeyboard = function (id) {
 	var self = this;
 	var menu = self.menu[id];
 	var keyboard = [];
-	if (!self.texts.bot_menu[menu.keyboard_path]) {
-		console.error('no menu for', menu.keyboard_path);
+	if (!self.texts.botMenu[menu.keyboardPath]) {
+		console.error('no menu for', menu.keyboardPath);
 		return keyboard;
 	}
-	self.texts.bot_menu[menu.keyboard_path].forEach(function(menu_line, i) {
+	self.texts.botMenu[menu.keyboardPath].forEach(function(menuLine, i) {
 		keyboard[i] = [];
-		menu_line.forEach(function(menu_item) {
-			var new_key_piece = menu.texts.accept_commands[menu_item];
-			if (new_key_piece) {
-				keyboard[i].push(new_key_piece);
+		menuLine.forEach(function(menuItem) {
+			var newKeyPiece = menu.texts.acceptCommands[menuItem];
+			if (newKeyPiece) {
+				keyboard[i].push(newKeyPiece);
 			} else {
-				console.error('no lang text for', menu_item);
+				console.error('no lang text for', menuItem);
 			}
 		});
 	});
-	if (menu.request_location) {
-		delete menu.request_location;
-		keyboard.unshift([{text: menu.texts.wait_for_geo_button_text, request_location: true}]);
+	if (menu.requestLocation) {
+		delete menu.requestLocation;
+		keyboard.unshift([{text: menu.texts.waitForGeoButtonText, request_location: true}]);
 	}
-	if (menu.request_contact) {
-		delete menu.request_contact;
-		keyboard.unshift([{text: menu.texts.wait_for_contact_button_text, request_contact: true}]);
+	if (menu.requestContact) {
+		delete menu.requestContact;
+		keyboard.unshift([{text: menu.texts.waitForContactButtonText, request_contact: true}]);
 	}
-	if (menu.keyboard_addOn) {
-		keyboard.unshift(menu.keyboard_addOn);
-		delete menu.keyboard_addOn;
+	if (menu.keyboardAddOn) {
+		keyboard.unshift(menu.keyboardAddOn);
+		delete menu.keyboardAddOn;
 	}
 	return keyboard;
 };
@@ -175,28 +175,28 @@ Bot.prototype.parseText = function (id) {
 	var self = this;
 	var menu = self.menu[id];
 	menu.answer = '';
-	var user_text = ((menu.msg && menu.msg.text) ? menu.msg.text : '').replace(/^\//, '').toLowerCase();
+	var userText = ((menu.msg && menu.msg.text) ? menu.msg.text : '').replace(/^\//, '').toLowerCase();
 	var command = '';
 	if (!menu.texts) {
 		console.error('use menu.noNextQueue');
 		return;
 	}
-	if ( !menu.texts.accept_commands ) {
+	if ( !menu.texts.acceptCommands ) {
 		console.error('no texts on lang,', menu.lang);
 		return;
 	}
-	Object.keys(menu.texts.accept_commands).forEach(function (key) {
-		var elem = menu.texts.accept_commands[key].toLowerCase();
-		if (key == user_text || elem == user_text ) {
+	Object.keys(menu.texts.acceptCommands).forEach(function (key) {
+		var elem = menu.texts.acceptCommands[key].toLowerCase();
+		if (key == userText || elem == userText ) {
 			command = key;
 		}
 	});
-	/** @param {Object} menu.drop_command */
-	command = (menu.drop_command) ? '' : command;
-	delete menu.drop_command;
+	/** @param {Object} menu.dropCommand */
+	command = (menu.dropCommand) ? '' : command;
+	delete menu.dropCommand;
 	menu.command = command;
-	menu.user_text = (menu.drop_user_text) ? '' : user_text;
-	delete menu.drop_user_text;
+	menu.userText = (menu.dropUserText) ? '' : userText;
+	delete menu.dropUserText;
 	self.on(menu.path, id);
 };
 
@@ -218,13 +218,13 @@ Bot.prototype.goToQueue = function (id) {
 	var menu = self.menu[id];
 	var tmp = {
 		path: menu.path,
-		last_ping: menu.last_ping,
-		on_work: false,
-		keyboard_path: menu.keyboard_path,
+		lastPing: menu.lastPing,
+		onWork: false,
+		keyboardPath: menu.keyboardPath,
 		lang: menu.lang,
-		save_queue_params: menu.save_queue_params
+		saveQueueParams: menu.saveQueueParams
 	};
-	Object.keys(self.save_queue_params).forEach(function (param) {
+	Object.keys(self.saveQueueParams).forEach(function (param) {
 		tmp[param] = menu[param];
 	});
 	self.menu[id] = tmp;
@@ -233,13 +233,13 @@ Bot.prototype.goToQueue = function (id) {
 
 Bot.prototype.sendClearMessage = function (opts) {
 	var self = this;
-	if (opts.id == opts.to_id) {
-		opts.user_text += opts.himself_text;
+	if (opts.id == opts.toId) {
+		opts.userText += opts.himselfText;
 	}
 	self.bot.sendMessage({
-		chat_id: opts.to_id,
+		chat_id: opts.toId,
 		action: 'typing',
-		text: opts.user_text || '',
+		text: opts.userText || '',
 		parse_mode: 'HTML'
 	});
 };
@@ -250,14 +250,14 @@ Bot.prototype.sendMessage = function (id, callback) {
 	self.bot.sendMessage({
 			chat_id: menu.msg.chat.id,
 			action: 'typing',
-			text: menu.answer || menu.texts.bot_error,
+			text: menu.answer || menu.texts.botError,
 			parse_mode: 'HTML',
 			reply_markup: {
 					keyboard: menu.keyboard,
-					resize_keyboard: self.vars.resize_keyboard
+					resize_keyboard: self.vars.resizeKeyboard
 				}
 		}, function(err, msg) {
-			menu.last_msg = msg;
+			menu.lastMsg = msg;
 			if (!err && callback) {
 				console.log(1, msg.message_id);
 				callback.call(self, id, msg.message_id);
@@ -277,8 +277,8 @@ Bot.prototype.on = function (menu, callback) {
 	if (typeof callback == 'function') { // init on
 		on[menu] = callback;
 	} else { // call, id = callback
-		if (on['before_action']) {
-			on['before_action'].call(self, callback);
+		if (on['beforeAction']) {
+			on['beforeAction'].call(self, callback);
 		}
 		if (on[menu]) {
 			on[menu].call(self, callback);
@@ -287,8 +287,8 @@ Bot.prototype.on = function (menu, callback) {
 				on['default'].call(self, callback);
 			}
 		}
-		if (on['after_action']) {
-			on['after_action'].call(self, callback);
+		if (on['afterAction']) {
+			on['afterAction'].call(self, callback);
 		}
 	}
 	return this;
